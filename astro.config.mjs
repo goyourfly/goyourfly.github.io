33893af3186a6e84e307f4a25efe5e9475f4a1d9
AiYoYo <goyourfly@gmail.com>
2024-07-08 16:45:16 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://goyourfly.github.io",
-  base: "/",
+  // base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
